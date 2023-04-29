fa7c4d3dad4685b62291498a4af060074fa84105
anikpuranik <aniketpuranik1995@gmail.com>
2023-04-29 20:30:05 +0530

    Adding morning quotes

--- app.py
+++ app.py
@@ -10,17 +10,21 @@ def home():
     '''Hmme page for the quotation-app'''
     return "Hello, This is flask app to test the quotes app"
 
-@app.route('/quotes')
-def quotes():
+@app.route('/onepiece_quotes')
+def onepiece_quotes():
     '''Function return the quotes for the day'''
-    lis = ("Scars On The Back Are A Swordsman's Shame. - Roronoa Zoro", 
+    quotes = ("Scars On The Back Are A Swordsman's Shame. - Roronoa Zoro", 
             "How Can We Look Any Of You In The Eyes And Say That Wano Is Safe With Us? - Kozuki Momonosuke", 
             "The New Era Of Daring Ones Is Coming With An Unstoppable Swell!- Trafalgar D. Law", 
             "People’s Dreams... Have No Ends — Marshall D. Teach",
             "One Piece Does Exist! — Edward 'Whitebeard' Newgate")
-    output = lis[randint(0, len(lis))]
-    print(output)
-    return output
+    return lis[randint(0, len(lis))]
+
+@app.route('/morning_quotes')
+def morning_quotes():
+    '''Quotes For Good Morning'''
+    quotes = ("Good Morning!", "Have a nice day", "Good luck for today", "You will have a charming day")
+    return lis[randint(0, len(quotes))]
 
 if __name__ =='__main__':
     app.run(port=8000, debug = True)
