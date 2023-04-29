19b8bb1151901d2a69b4929166df75cf42ba06b9
anikpuranik <aniketpuranik1995@gmail.com>
2023-04-29 19:12:29 +0530

    Test Modification

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from random import randint
 from flask import Flask
 
-app = Flask(__name__) #creating the Flask class object
+app = Flask(__name__) #creating Flask class object
 
 @app.route('/') #decorator defines the
 def home():
