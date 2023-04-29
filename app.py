2d2846e36741a1d372713d531e23ae36ffdaf799
anikpuranik <aniketpuranik1995@gmail.com>
2023-04-29 12:25:39 +0530

    Dockerizing the app

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-#!/usr/bin/env python3
-from flask import Flask  
-from random import randint
-
-app = Flask(__name__) #creating the Flask class object   
+#Import flask module
+from flask import Flask
  
-@app.route('/') #decorator drfines the   
-def home():  
-    return "Hello, This is flask app to test the quotes app";  
-
-@app.route('/quotes')
-def quotes():
-    lis = ("Father", "Mother", "Son", "GrandFather", "GrandMother")
-    output = lis[randint(0, len(lis))]
-    print(output)
-    return output
-
-if __name__ =='__main__':  
-    app.run(port=8000, debug = True)
+app = Flask(__name__)
+  
+@app.route('/')
+def index():
+    return 'Hello to Flask!'
+       
+# main driver function
+if __name__ == "__main__":
+    app.run()
