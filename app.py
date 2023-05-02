fd012057d3138125138bc29451aabfd410802883
Aniket Puranik <aniketpuranik1995@gmail.com>
2023-05-02 21:38:56 +0530

    appending ".json" extension to url

--- app.py
+++ app.py
@@ -10,21 +10,21 @@ def home():
     '''Hmme page for the quotation-app'''
     return "Hello, This is flask app to test the quotes app"
 
-@app.route('/onepiece_quotes')
+@app.route('/onepiece_quotes.json')
 def onepiece_quotes():
     '''Function return the quotes for the day'''
-    quotes = ("Scars On The Back Are A Swordsman's Shame. - Roronoa Zoro",
+    one_quotes = ("Scars On The Back Are A Swordsman's Shame. - Roronoa Zoro",
             "How Can We Look Any Of You In The Eyes And Say That Wano Is Safe With Us? - Kozuki Momonosuke",
             "The New Era Of Daring Ones Is Coming With An Unstoppable Swell!- Trafalgar D. Law",
             "People’s Dreams... Have No Ends — Marshall D. Teach",
             "One Piece Does Exist! — Edward 'Whitebeard' Newgate")
-    return {"quote":quotes[randint(0, len(quotes))]}
+    return {"quote":one_quotes[randint(0, len(one_quotes))]}
 
-@app.route('/morning_quotes')
+@app.route('/morning_quotes.json')
 def morning_quotes():
     '''Quotes For Good Morning'''
-    quotes = ("Good Morning!", "Have a nice day", "Good luck for today", "You will have a charming day")
-    return {"quote":quotes[randint(0, len(quotes))]}
+    morning_quotes = ("Good Morning!", "Have a nice day", "Good luck for today", "You will have a charming day")
+    return {"quote":morning_quotes[randint(0, len(morning_quotes))]}
 
 if __name__ =='__main__':
     app.run(port=8000, debug = True)
