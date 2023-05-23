1ca2795cb1757a371efd084876ed0578394f6f7c
Anik <aniketpuranik1995@gmail.com>
2023-05-23 20:30:36 +0530

    Modifiy

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 '''This is a demo of the quotation of the app'''
 from random import randint
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__) #creating Flask class object
 
 @app.route('/') #decorator defines the
+@app.route('/home')
 def home():
     '''Hmme page for the quotation-app'''
     home_page_message = "Hello, This is flask app to test the quotes app"
@@ -14,8 +15,6 @@ def home():
 def read_quote_from_file(path):
     with open(path, "r") as file:
         quote = file.read().split("\n")[:-1]
-    print("Lenght of the quotes:", len(quote))
-    print(quote)
     return quote
 
 @app.route('/onepiece_quotes.json')
@@ -23,11 +22,11 @@ def onepiece_quotes():
     '''Function return the quotes for the day'''
     path = 'db/onepiece.txt'
     one_quotes = read_quote_from_file(path)
-    return {"quote":one_quotes[randint(0, len(one_quotes)-1)]}
+    return {"quote":one_quotes[randint(0, len(one_quotes))]}
 
 @app.route('/morning_quotes.json')
 def morning_quotes():
     '''Quotes For Good Morning'''
     path = 'db/morning.txt'
     morning_quotes = read_quote_from_file(path) 
-    return {"quote":morning_quotes[randint(0, len(morning_quotes)-1)]}
+    return {"quote":morning_quotes[randint(0, len(morning_quotes))]}
